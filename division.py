4ec21bccfe178e089d0f867819b4ab6e81bd0503
Geraldo Braho <gbraho@na.edu>
2016-07-11 19:06:29 -0500

    first commit

---
+++ division.py
@@ -0,0 +1,15 @@
+
+#Task 
+#Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+
+Y#ou don't need to perform any rounding or formatting operations.
+
+
+
+from __future__ import division
+
+a=int(raw_input())
+b=int(raw_input())
+
+print a // b
+print a / b
