8bfdd9405189c698b78957e2b987d2e0970a9f6f
Geraldo Braho <gbraho@na.edu>
2016-09-07 21:35:02 -0500

    .

---
+++ Algorithm/graph theory/gameOfStones.py
@@ -0,0 +1,13 @@
+numberOfTestCases = int(raw_input())
+op = []
+for i in range(numberOfTestCases):
+    availableStones = int(raw_input())
+    result = [availableStones % 7 in [0, 1]]
+    if result is None:
+        pass
+    elif result == [True]:
+        op.append("Second")
+    else:
+        op.append("First")
+for i in op:
+    print i
