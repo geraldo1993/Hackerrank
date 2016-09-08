d2dcb691db0af6b6f9f1e1ccd7912270d4b2a735
Geraldo Braho <gbraho@na.edu>
2016-09-07 20:10:44 -0500

    Adding new folder

---
+++ Algorithm/Bit manipulation/Lonely_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/py
+def lonelyinteger(a):
+    answer = 0
+    for x in a:
+        answer=answer^x
+    return answer
+if __name__ == '__main__':
+    a = input()
+    b = map(int, raw_input().strip().split(" "))
+    print lonelyinteger(b)
