448b5375752947c85b6cff53a42e86a28d3c89c0
Geraldo Braho <gbraho@na.edu>
2017-03-02 20:23:45 -0600

    Delete fibonaciModified.py

--- Algorithm/Dynamic Programming /fibonaciModified.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-t0, t1, n = map(int, raw_input().strip().split(' '))
-
-for i in xrange(n-2):
-    t1, t0 = t0 + t1*t1, t1
-
-print(t1)
